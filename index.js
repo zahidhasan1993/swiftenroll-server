9e024a3965b5ea89aa4ccc736bbc0eefdb0f6155
devZahid <officialzahidhasan19932021@gmail.com>
2023-07-24 22:46:14 +0600

    adding some routes and get api

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
-
+const port = process.env.PORT || 5000;
 
 //middleware
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 //mongo connections
@@ -32,11 +31,33 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+    const database = client.db("swiftenroll");
+    const colleges = database.collection('colleges');
+
+    //apis
+
+    app.get('/colleges', async(req,res) => {
+        const cursor = colleges.find();
+        const result = await cursor.toArray();
+
+        res.send(result)
+    })
+
+    app.get('/college/:id', async(req,res) => {
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id)};
+      const result = await colleges.findOne(query);
+
+      res.send(result)
+    })
+
+
+    //mongo connect sure
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -45,7 +66,7 @@ run().catch(console.dir);
 //server routes connection
 
 app.get('/', (req,res) => {
-    res.send('Welcome to WhizzyWheels Server')
+    res.send('Welcome to SwiftsEnroll Server')
 })
 
 app.listen(port, () => {
